7d2103f6e25aa3de3b101a28a685e2f15b97344d
HAmid <hamidprince823@gmail.com>
2022-09-14 16:31:09 +0500

    configration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "300px",
+      md: "769px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
